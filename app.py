402c282bf2be22f56070e9e5e13da91ba317259c
bountyshark62-web <bountyshark62@gmail.com>
2026-06-23 02:57:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from hero import Hero
 
 # --- НАСТРОЙКА БАЗЫ ДАННЫХ SUPABASE ---
 SUPABASE_URL = "https://supabase.co"
-SUPABASE_KEY = "sb_publishable_lhFtEl3aWOxrovhgVjYqYA_L2Sx5DEW"
+SUPABASE_KEY = "ВСТАВЬ_СЮДА_СКОПИРОВАННЫЙ_КЛЮЧ"
 
 # Инициализируем подключение к базе данных
 @st.cache_resource
